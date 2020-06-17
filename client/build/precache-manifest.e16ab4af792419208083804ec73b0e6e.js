b67083790656ee03abf1c57b5fc90726ed3b9e42
kkd <kkd@omegasoftware.eu>
2020-06-17 22:40:58 +0300

    mobile

---
+++ client/build/precache-manifest.e16ab4af792419208083804ec73b0e6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b826055981957e76b2272d8b113edc56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e61bff80099e1d2f786",
+    "url": "/static/css/2.bf41a979.chunk.css"
+  },
+  {
+    "revision": "eee1f44107126e980435",
+    "url": "/static/css/main.569242fb.chunk.css"
+  },
+  {
+    "revision": "7e61bff80099e1d2f786",
+    "url": "/static/js/2.2d27b011.chunk.js"
+  },
+  {
+    "revision": "8e2129b192ae19668a0e21de1f174455",
+    "url": "/static/js/2.2d27b011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee1f44107126e980435",
+    "url": "/static/js/main.69f44123.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
